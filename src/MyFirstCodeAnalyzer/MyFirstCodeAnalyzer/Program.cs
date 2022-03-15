3953daf9268590d6929cda1a0c5eda1649ec5160
Pfeff Ákos György <ike86@users.noreply.github.com>
2022-03-15 19:40:04 +0100

    Clean up `Program`
    - extract methods
      - `GetVisualStudioInstance`
      - `CreateMsBuildWorkspace`
      - `OpenSolution`
      - ˙Analyze`
    - remove redundant comments
    - replace use of command line argument with hard coded constant `SolutionFilePath`
    - create solution for analysis target
    
    At this point, running `Program` produces this output
    ```
    ...
    Using MSBuild at 'C:\Program Files\dotnet\sdk\6.0.200\' to load projects.
    Loading solution '..\..\..\..\MyFirstCodeAnalyzer.Target\MyFirstCodeAnalyzer.Target.sln'
    Evaluate        0:00.4234130    MyFirstCodeAnalyzer.Target.csproj
    Build           0:00.4520825    MyFirstCodeAnalyzer.Target.csproj
    Resolve         0:00.2248738    MyFirstCodeAnalyzer.Target.csproj (net6.0)
    Finished loading solution '..\..\..\..\MyFirstCodeAnalyzer.Target\MyFirstCodeAnalyzer.Target.sln'
    
    Analyzing the solution...
    
    Process finished with exit code 0.
    ```

--- src/MyFirstCodeAnalyzer/MyFirstCodeAnalyzer/Program.cs
+++ src/MyFirstCodeAnalyzer/MyFirstCodeAnalyzer/Program.cs
@@ -1,90 +1,110 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Build.Locator;
+﻿using Microsoft.Build.Locator;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Symbols;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.MSBuild;
-using Microsoft.CodeAnalysis.Text;
 
-namespace MSBuildWorkspaceExample
+namespace MyFirstCodeAnalyzer;
+
+public static class Program
 {
-    class Program
+    private const string SolutionFilePath = @"..\..\..\..\MyFirstCodeAnalyzer.Target\MyFirstCodeAnalyzer.Target.sln";
+
+    public static async Task Main()
     {
-        static async Task Main(string[] args)
+        var instance = GetVisualStudioInstance();
+        using var workspace = CreateMsBuildWorkspace(instance);
+        var solution = await OpenSolution(workspace);
+        await Analyze(solution);
+    }
+
+    private static VisualStudioInstance GetVisualStudioInstance()
+    {
+        var visualStudioInstances =
+            MSBuildLocator.QueryVisualStudioInstances().ToArray();
+        var instance = visualStudioInstances.Length == 1
+            ? visualStudioInstances[0]
+            : SelectVisualStudioInstance(visualStudioInstances);
+
+        Console.WriteLine(
+            $"Using MSBuild at '{instance.MSBuildPath}' to load projects.");
+        return instance;
+    }
+
+    private static MSBuildWorkspace CreateMsBuildWorkspace(
+        VisualStudioInstance instance)
+    {
+        // NOTE: Be sure to register an instance with the MSBuildLocator 
+        //       before calling MSBuildWorkspace.Create()
+        //       otherwise, MSBuildWorkspace won't MEF compose.
+        MSBuildLocator.RegisterInstance(instance);
+
+        var workspace = MSBuildWorkspace.Create();
+        workspace.WorkspaceFailed +=
+            (_, e) => Console.WriteLine(e.Diagnostic.Message);
+        return workspace;
+    }
+
+    private static async Task<Solution> OpenSolution(MSBuildWorkspace workspace)
+    {
+        Console.WriteLine($"Loading solution '{SolutionFilePath}'");
+
+        // Attach progress reporter so we print projects as they are loaded.
+        var solution =
+            await workspace.OpenSolutionAsync(
+                SolutionFilePath,
+                new ConsoleProgressReporter());
+        Console.WriteLine($"Finished loading solution '{SolutionFilePath}'");
+        return solution;
+    }
+
+    private static VisualStudioInstance SelectVisualStudioInstance(
+        VisualStudioInstance[] visualStudioInstances)
+    {
+        Console.WriteLine(
+            "Multiple installs of MSBuild detected please select one:");
+        for (int i = 0; i < visualStudioInstances.Length; i++)
         {
-            // Attempt to set the version of MSBuild.
-            var visualStudioInstances = MSBuildLocator.QueryVisualStudioInstances().ToArray();
-            var instance = visualStudioInstances.Length == 1
-                // If there is only one instance of MSBuild on this machine, set that as the one to use.
-                ? visualStudioInstances[0]
-                // Handle selecting the version of MSBuild you want to use.
-                : SelectVisualStudioInstance(visualStudioInstances);
-
-            Console.WriteLine($"Using MSBuild at '{instance.MSBuildPath}' to load projects.");
-
-            // NOTE: Be sure to register an instance with the MSBuildLocator 
-            //       before calling MSBuildWorkspace.Create()
-            //       otherwise, MSBuildWorkspace won't MEF compose.
-            MSBuildLocator.RegisterInstance(instance);
-            
-            using (var workspace = MSBuildWorkspace.Create())
-            {
-                // Print message for WorkspaceFailed event to help diagnosing project load failures.
-                workspace.WorkspaceFailed += (o, e) => Console.WriteLine(e.Diagnostic.Message);
-
-                var solutionPath = args[0];
-                Console.WriteLine($"Loading solution '{solutionPath}'");
-
-                // Attach progress reporter so we print projects as they are loaded.
-                var solution = await workspace.OpenSolutionAsync(solutionPath, new ConsoleProgressReporter());
-                Console.WriteLine($"Finished loading solution '{solutionPath}'");
-
-                // TODO: Do analysis on the projects in the loaded solution
-            }
+            Console.WriteLine($"Instance {i + 1}");
+            Console.WriteLine($"    Name: {visualStudioInstances[i].Name}");
+            Console.WriteLine(
+                $"    Version: {visualStudioInstances[i].Version}");
+            Console.WriteLine(
+                $"    MSBuild Path: {visualStudioInstances[i].MSBuildPath}");
         }
-        
-        private static VisualStudioInstance SelectVisualStudioInstance(VisualStudioInstance[] visualStudioInstances)
+
+        while (true)
         {
-            Console.WriteLine("Multiple installs of MSBuild detected please select one:");
-            for (int i = 0; i < visualStudioInstances.Length; i++)
+            var userResponse = Console.ReadLine();
+            if (int.TryParse(userResponse, out int instanceNumber)
+                && instanceNumber > 0
+                && instanceNumber <= visualStudioInstances.Length)
             {
-                Console.WriteLine($"Instance {i + 1}");
-                Console.WriteLine($"    Name: {visualStudioInstances[i].Name}");
-                Console.WriteLine($"    Version: {visualStudioInstances[i].Version}");
-                Console.WriteLine($"    MSBuild Path: {visualStudioInstances[i].MSBuildPath}");
+                return visualStudioInstances[instanceNumber - 1];
             }
 
-            while (true)
-            {
-                var userResponse = Console.ReadLine();
-                if (int.TryParse(userResponse, out int instanceNumber) &&
-                    instanceNumber > 0 &&
-                    instanceNumber <= visualStudioInstances.Length)
-                {
-                    return visualStudioInstances[instanceNumber - 1];
-                }
-                Console.WriteLine("Input not accepted, try again.");
-            }
+            Console.WriteLine("Input not accepted, try again.");
         }
+    }
+    
+    private static async Task Analyze(Solution solution)
+    {
+        Console.WriteLine();
+        Console.WriteLine("Analyzing the solution...");
+        _ = solution;
+        await Task.CompletedTask;
+    }
 
-        private class ConsoleProgressReporter : IProgress<ProjectLoadProgress>
+    private class ConsoleProgressReporter : IProgress<ProjectLoadProgress>
+    {
+        public void Report(ProjectLoadProgress loadProgress)
         {
-            public void Report(ProjectLoadProgress loadProgress)
+            var projectDisplay = Path.GetFileName(loadProgress.FilePath);
+            if (loadProgress.TargetFramework != null)
             {
-                var projectDisplay = Path.GetFileName(loadProgress.FilePath);
-                if (loadProgress.TargetFramework != null)
-                {
-                    projectDisplay += $" ({loadProgress.TargetFramework})";
-                }
-
-                Console.WriteLine($"{loadProgress.Operation,-15} {loadProgress.ElapsedTime,-15:m\\:ss\\.fffffff} {projectDisplay}");
+                projectDisplay += $" ({loadProgress.TargetFramework})";
             }
+
+            Console.WriteLine(
+                $"{loadProgress.Operation,-15} {loadProgress.ElapsedTime,-15:m\\:ss\\.fffffff} {projectDisplay}");
         }
     }
 }
