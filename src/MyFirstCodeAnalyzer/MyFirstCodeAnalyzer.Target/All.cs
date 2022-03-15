0f84b6939bd7d8cec4514b5db01ccc6fe00eb1c9
Pfeff Ákos György <ike86@users.noreply.github.com>
2022-03-15 18:43:54 +0100

    Add initial code sample for my first code analyzer
    - create solution
    - create project to analyze

---
+++ src/MyFirstCodeAnalyzer/MyFirstCodeAnalyzer.Target/All.cs
@@ -0,0 +1,15 @@
+﻿namespace MyFirstCodeAnalyzer.Target;
+
+public record SessionId(string Value);
+
+public class SessionIdUser
+{
+    private readonly string sessionId;
+
+    public SessionIdUser(string sessionId)
+    {
+        this.sessionId = sessionId;
+    }
+
+    public string SessionId => $"Session id: {sessionId}";
+}
